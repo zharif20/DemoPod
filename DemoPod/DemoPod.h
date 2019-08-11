109345bd01d9798bd1a75c3639ff3a692955ce68
Zharif.Hadi @ 123RF <zharif.hadi@123rf.com>
2019-08-11 23:44:14 +0800

    Initial Commit

---
+++ DemoPod/DemoPod.h
@@ -0,0 +1,19 @@
+//
+//  DemoPod.h
+//  DemoPod
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 11/08/2019.
+//  Copyright © 2019 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoPod.
+FOUNDATION_EXPORT double DemoPodVersionNumber;
+
+//! Project version string for DemoPod.
+FOUNDATION_EXPORT const unsigned char DemoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoPod/PublicHeader.h>
+
+
